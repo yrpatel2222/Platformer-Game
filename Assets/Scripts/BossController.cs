4ca8e214b88a3befe62de9b85abfb0c0878d08a2
yrpatel2222 <yrpatel2222@gmail.com>
2023-07-29 21:32:58 -0400

    Game Commit

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+        public float jumpForce = 8f;
+        public float jumpInterval = 3f;
+
+        private Rigidbody2D rb;
+        private bool canJump = true;
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            // Start the coroutine for automatic jumps
+            StartCoroutine(AutoJump());
+        }
+
+        private void Update()
+        {
+            // Check for manual jump (you can implement boss's jump controls here if needed)
+            if (canJump)
+            {
+                Jump();
+            canJump = false;
+            }
+        }
+
+        private void Jump()
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canJump = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        private IEnumerator AutoJump()
+        {
+            while (true)
+            {
+                // Wait for the specified interval
+                yield return new WaitForSeconds(jumpInterval);
+                // Call the Jump() function to make the boss jump automatically
+                Jump();
+            }
+        }
 }
