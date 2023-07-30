d90bbf269ffa967f8185462a5f7fcc34a32b802a
ProgramRush <skandavk@gmail.com>
2023-07-29 22:19:59 -0400

    Game Commit

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -4,50 +4,57 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
-        public float jumpForce = 8f;
-        public float jumpInterval = 3f;
+    public float jumpForce = 8f;
+    public float jumpInterval = 3f; // Time interval between jumps (in seconds)
 
-        private Rigidbody2D rb;
-        private bool canJump = true;
+    private Rigidbody2D rb;
+    private bool canJump = true;
+    public GameObject startPoint;
+    public GameObject Player;
 
-        private void Start()
+    private float jumpTimer = 0f;
+    private bool isJumping = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        // Check for manual jump (you can implement boss's jump controls here if needed)
+        if (canJump && !isJumping)
         {
-            rb = GetComponent<Rigidbody2D>();
-            // Start the coroutine for automatic jumps
-            StartCoroutine(AutoJump());
-        }
+            jumpTimer += Time.deltaTime;
 
-        private void Update()
-        {
-            // Check for manual jump (you can implement boss's jump controls here if needed)
-            if (canJump)
+            if (jumpTimer >= jumpInterval)
             {
                 Jump();
-            canJump = false;
+                canJump = false;
+                jumpTimer = 0f;
             }
         }
+    }
 
-        private void Jump()
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        isJumping = true;
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             canJump = true;
+            isJumping = false;
+        }
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Player.transform.position = startPoint.transform.position;
+            FindObjectOfType<LivesUI>().DecreaseLives();
         }
     }
-
-        private IEnumerator AutoJump()
-        {
-            while (true)
-            {
-                // Wait for the specified interval
-                yield return new WaitForSeconds(jumpInterval);
-                // Call the Jump() function to make the boss jump automatically
-                Jump();
-            }
-        }
 }
